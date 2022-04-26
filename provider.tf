4429d9d897c3854af8195a35491b4b04c65fe0a8
prashantkat1980 <59108712+prashantkat1980@users.noreply.github.com>
2022-04-26 14:55:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region                   = "us-east-1"
-  access_key = "AKIAV3YOLXVGWOLV2BNI"
-  secret_key = "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"
+ 
   profile                  = "default"
 
 }
